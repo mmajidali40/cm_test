4ddc4652d4e846565abb011723f55c0b7d89f538
majid <mmajidali40@gmail.com>
2018-02-28 15:05:41 +0500

    completed

--- routes/scrap.js
+++ routes/scrap.js
@@ -1,40 +1,109 @@
 
 var express = require('express');
 var router = express.Router();
-
+var async = require('async');
+var scrapeIt = require("scrape-it");
 
 router.get('/', function(req, res, next) {
-//    var queryPrameters = Object.keys(req.query);
+
   var query = req.query;
+  var addresses;
   if(Object.keys(query).length == 0 || query.address == null)
   {
-    console.log('no add');
-  } else {
-    console.log(Array.isArray(query.address));
+    return res.send('no link is provided');
   }
-  const scrapeIt = require("scrape-it");
-  var arr= ["https://ionicabizau.net","https://www.linkedin.com"];
-  arr.forEach(function(e){
-    console.log(scrape(e));
-  });
+  if(!Array.isArray(query.address)) {
+    addresses = [];
+    addresses.push(query.address);
+  } else {
+    addresses = query.address;    
+  }
+  callbackFlow(addresses, res);
+  // asyncFlow(addresses, res);
+  // promisFlow(addresses, res);
+});
 
-  //res.send('yess');
-  
-  function scrape(url) {
-    var value = true;
-    var returnData;
-    scrapeIt(url, {
+module.exports = router;
+
+function callbackFlow(addresses, res) {
+  var result = [];
+  addresses.forEach(function(add){
+    scrapeIt(add, {
+      title: "title"
+    }, function(err, data) {
+        var obj = {
+          url: add,
+          title: ''
+        };
+        if(err) {
+        obj.title = 'NO RESPONSE';
+        result.push( obj );
+      } else {
+        console.log(`Status Code: ${data.response.statusCode}`)
+        console.log(data.data);
+        obj.title = data.data.title;
+        result.push( obj );
+      }
+      if(result.length >= addresses.length) {
+        res.render('scrap', { result: result});
+      }
+    });
+  });
+}
+
+function promisFlow(addresses, res) {
+  var promiseArray = [];
+  var result = [];
+  addresses.forEach(function(url) {
+    var obj = {
+      url: url,
+      title: ''
+    };
+    promiseArray.push( scrapeIt(url, {
       title: "title"
     }).then(({ data, response }) => {
       console.log(`Status Code: ${response.statusCode}`)
       console.log(data);
-      returnData = data.title;
-      value = false;
-    })
-    //while(value);
-    return returnData;
-  }
+      obj.title = data.title;
+      result.push( obj );
+    }).catch( (error) => {
+      obj.title = 'NO RESPONSE';
+      result.push( obj );
+    }) );
+  });
+  Promise.all(promiseArray).then( () => {
+    res.render('scrap', { result: result});
+  });
+}
 
-});
-
-module.exports = router;
+function asyncFlow(addresses, res) {
+  var result = [];
+  var index = 0;
+  async.whilst(
+      function() { return index < addresses.length; },
+      function(callback) {
+        index++;
+        scrapeIt(addresses[index-1], {
+          title: "title"
+        }, function(err, data) {
+          var obj = {
+              url: addresses[index-1],
+              title: ''
+            };
+            if(err) {
+            obj.title = 'NO RESPONSE';
+            result.push( obj );
+          } else {
+            console.log(`Status Code: ${data.response.statusCode}`)
+            console.log(data.data);
+            obj.title = data.data.title;
+            result.push( obj );
+          }
+          callback(null, index);
+        });
+      },
+      function (err, n) {
+        res.render('scrap', { result: result});
+      }
+  );
+}
